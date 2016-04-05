edab54d176fe10d31f0ce01e5f768a5364bbad05
amsatique <amsatique@gmail.com>
2016-04-05 17:00:14 +0200

    Clear

--- install.sh
+++ install.sh
@@ -4,13 +4,15 @@ echo "sudo requis? [y/n]"
 read admin
 if [ $admin = y ]
 	then admin=sudo
+else admin=
 fi
+
 #==============Update==============
 clear
 echo "update ? [y/n]"
 read update
 if [ $update = y ]
-	then $admin apt-get update && $admin apt-get upgrade
+	then $admin apt-get update && $admin apt-get -y upgrade
 fi
 
 #==============Basics==============
@@ -18,7 +20,7 @@ clear
 echo "Install basics ? [y/n]"
 read basics
 if [ $basics = y ]
-	then $admin apt-get install -y wget curl git htop
+	then $admin apt-get install -y wget curl git htop vim
 fi
 
 #==============Docker==============
@@ -29,16 +31,22 @@ if [ $install_docker = y ]
 	then $admin apt-get purge lxc-docker* && $admin apt-get purge docker.io* && $admin apt-get update
 		 $admin apt-get install apt-transport-https ca-certificates
 		 $admin apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-		 $admin echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list
+		 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | $admin tee /etc/apt/sources.list.d/docker.list
 		 $admin apt-get update && $admin apt-cache policy docker-engine
 		 $admin apt-get update
-		 $admin apt-get install docker-engine && $admin service docker start
+		 $admin apt-get install -y docker-engine && $admin service docker start
+		 echo "Docker has been launched"
+		 sleep 3
 fi
+
+
 
 #===============Tmux================
 clear
 echo "installer tmux ? [y/n]"
 read install_tmux
 if [ $install_tmux = y ]
-	then $admin apt-get install tmux
+	then $admin apt-get install -y tmux
+	echo "Tmux has been installed"
+sleep 3
 fi
